1616bc0495399db679f911224e9609191792d36b
NoriAlafa <alafanori@gmail.com>
2022-03-22 12:20:13 +0700

    [EDIT]footer

--- app/Views/user/laman_depan.php
+++ app/Views/user/laman_depan.php
@@ -119,12 +119,11 @@
         <div class="row mb-5">
           <div class="col-md">
             <div class="ftco-footer-widget mb-4">
-              <h2 class="ftco-heading-2">Deluxe Hotel</h2>
+              <h2 class="ftco-heading-2">FAHOTEL</h2>
               <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
               <ul class="ftco-footer-social list-unstyled float-md-left float-lft mt-5">
-                <li class="ftco-animate"><a href="#"><span class="icon-twitter"></span></a></li>
-                <li class="ftco-animate"><a href="#"><span class="icon-facebook"></span></a></li>
-                <li class="ftco-animate"><a href="#"><span class="icon-instagram"></span></a></li>
+                <li class="ftco-animate"><a href="https://twitter.com/Nori_Alafaa" target="_blank"><span class="icon-twitter"></span></a></li>
+                <li class="ftco-animate"><a href="https://facebook.com/profile.php?id=100016880164097" target="_blank"><span class="icon-facebook"></span></a></li>
               </ul>
             </div>
           </div>
@@ -132,10 +131,10 @@
             <div class="ftco-footer-widget mb-4 ml-md-5">
               <h2 class="ftco-heading-2">Useful Links</h2>
               <ul class="list-unstyled">
-                <li><a href="#" class="py-2 d-block">Blog</a></li>
-                <li><a href="#" class="py-2 d-block">Rooms</a></li>
-                <li><a href="#" class="py-2 d-block">Amenities</a></li>
-                <li><a href="#" class="py-2 d-block">Gift Card</a></li>
+                <li><a href="" class="py-2 d-block">Blog</a></li>
+                <li><a href="/kamarhotel" class="py-2 d-block">Rooms</a></li>
+                <li><a href="" class="py-2 d-block">Amenities</a></li>
+                <li><a href="" class="py-2 d-block">Gift Card</a></li>
               </ul>
             </div>
           </div>
@@ -143,10 +142,10 @@
              <div class="ftco-footer-widget mb-4">
               <h2 class="ftco-heading-2">Privacy</h2>
               <ul class="list-unstyled">
-                <li><a href="#" class="py-2 d-block">Career</a></li>
-                <li><a href="#" class="py-2 d-block">About Us</a></li>
-                <li><a href="#" class="py-2 d-block">Contact Us</a></li>
-                <li><a href="#" class="py-2 d-block">Services</a></li>
+                <li><a href="" class="py-2 d-block">Career</a></li>
+                <li><a href="" class="py-2 d-block">About Us</a></li>
+                <li><a href="" class="py-2 d-block">Contact Us</a></li>
+                <li><a href="" class="py-2 d-block">Services</a></li>
               </ul>
             </div>
           </div>
@@ -155,9 +154,9 @@
             	<h2 class="ftco-heading-2">Have a Questions?</h2>
             	<div class="block-23 mb-3">
 	              <ul>
-	                <li><span class="icon icon-map-marker"></span><span class="text">203 Fake St. Mountain View, San Francisco, California, USA</span></li>
-	                <li><a href="#"><span class="icon icon-phone"></span><span class="text">+2 392 3929 210</span></a></li>
-	                <li><a href="#"><span class="icon icon-envelope"></span><span class="text">info@yourdomain.com</span></a></li>
+	                <li><span class="icon icon-map-marker"></span><span class="text">Jl. Joyoboyo Timur No.146-127, Babadan, Sukorejo, Kec. Gurah, Kabupaten Kediri, Jawa Timur 64181</span></li>
+	                <li><a href=""><span class="icon icon-phone"></span><span class="text">+62 87779998268</span></a></li>
+	                <li><a href=""><span class="icon icon-envelope"></span><span class="text">erinrisnawati1922@gmail.com</span></a></li>
 	              </ul>
 	            </div>
             </div>
