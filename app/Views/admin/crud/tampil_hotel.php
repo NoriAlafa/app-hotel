9bec542616660f8c22ba9ce33585fc7a574aeadc
NoriAlafa <alafanori@gmail.com>
2022-04-14 15:19:19 +0700

    [ADD]Fix Bug

--- app/Views/admin/crud/tampil_hotel.php
+++ app/Views/admin/crud/tampil_hotel.php
@@ -15,7 +15,6 @@
                           <tr>
                             <th>ID</th>
                             <th>Nama Kamar</th>
-                            <th>Deskripsi</th>
                             <th>Tipe Kamar</th>
                             <th>Harga Kamar</th>
                             <th>Status</th>
@@ -31,7 +30,6 @@
                               <tr>
                                   <td><?=$no++?></td>
                                   <td><?=$kmr['nama_kamar']?></td>
-                                  <td><?=$kmr['deskripsi']?></td>
                                   <td><?=$kmr['tipe_kamar']?></td>
                                   <td>Rp <?=number_format($kmr['harga_kamar'])?></td>
                                   <td><?=$kmr['status_kamar']?></td>
